0210984e58a7de3b3526bdb12ad5e86489fa2dea
leonbornemann <leonbornemann@gmx.de>
2020-06-09 18:50:03 +0200

    first pointers to hazar

--- src/main/scala/de/hpi/dataset_versioning/db_synthesis/normalization/TestMain.java
+++ src/main/scala/de/hpi/dataset_versioning/db_synthesis/normalization/TestMain.java
@@ -1,10 +1,22 @@
 package de.hpi.dataset_versioning.db_synthesis.normalization;
 
+import de.hpi.dataset_versioning.data.DatasetInstance;
 import de.hpi.dataset_versioning.data.simplified.RelationalDataset;
+import de.hpi.dataset_versioning.io.IOService;
+
+import java.io.File;
+import java.time.LocalDate;
 
 public class TestMain {
 
     public static void main(String[] args) {
         System.out.println("Hi Hazar, this works");
+        System.out.println(IOService.socrataDir());
+        IOService.socrataDir_$eq("new SOcrata dir");
+        System.out.println(IOService.socrataDir());
+        File f = IOService.getJoinCandidateFile();
+        System.out.println(f.getAbsolutePath());
+        //you will need:
+        RelationalDataset ds = IOService.loadSimplifiedRelationalDataset(new DatasetInstance("your dataset id", LocalDate.parse("the latest version of this dataset")));
     }
 }
--- src/main/scala/de/hpi/dataset_versioning/oneshot/statisticis/ChangeHistogramBySubdomain.scala
+++ src/main/scala/de/hpi/dataset_versioning/oneshot/statisticis/ChangeHistogramBySubdomain.scala
@@ -1,9 +1,14 @@
 package de.hpi.dataset_versioning.oneshot.statisticis
 
+import java.io.PrintWriter
+import java.time.LocalDate
+
 import de.hpi.dataset_versioning.data.DatasetInstance
 import de.hpi.dataset_versioning.data.exploration.db_synthesis.ConditionalProbabilityExplorationMain.histories
 import de.hpi.dataset_versioning.data.history.DatasetVersionHistory
 import de.hpi.dataset_versioning.io.IOService
+
+import scala.collection.mutable.ArrayBuffer
 
 object ChangeHistogramBySubdomain extends App {
 
@@ -21,7 +26,24 @@ object ChangeHistogramBySubdomain extends App {
       else
         md.metadata(firstVersion).topLevelDomain
     }}
+  val pr = new PrintWriter("changeCountWithSubdomain.csv")
+  pr.println("subdomain,id,#changes,#deletes,firstInsert,latestChange,latestDelete")
+
+  def lastOrNan(value: ArrayBuffer[LocalDate]) = {
+    if(value.isEmpty) "NA" else value.last.format(IOService.dateTimeFormatter)
+  }
+
+  def firstOrNan(value: ArrayBuffer[LocalDate]) = if(value.isEmpty) "NA" else value.head.format(IOService.dateTimeFormatter)
+
   byDomain.foreach{case (k,map) => {
-    map.foreach(entry => println(s"$k,${entry._2.versionsWithChanges.size}"))
+    map.foreach(entry => {
+      val changes = entry._2.versionsWithChanges
+      val deletions = entry._2.deletions
+      val firstInsert = firstOrNan(changes.sortBy(_.toEpochDay))
+      val latestChange = lastOrNan(changes.sortBy(_.toEpochDay))
+      val latestDelete = lastOrNan(deletions.sortBy(_.toEpochDay))
+      pr.println(s"$k,${entry._1},${changes.size-1},${deletions.size},$firstInsert,$latestChange,$latestDelete}")
+    })
   }}
+  pr.close()
 }

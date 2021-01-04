f206d3ae19031d450044220f936e79af8a6cec90
leonbornemann <leonbornemann@gmx.de>
2021-01-04 10:31:40 +0100

    Beginning of new dedicated change counting main and decomposition completion (fixed)

--- src/main/scala/de/hpi/dataset_versioning/db_synthesis/baseline/decomposition/DecompositionCompletionMain.scala
+++ src/main/scala/de/hpi/dataset_versioning/db_synthesis/baseline/decomposition/DecompositionCompletionMain.scala
@@ -1,6 +1,6 @@
 package de.hpi.dataset_versioning.db_synthesis.baseline.decomposition
 
-import de.hpi.dataset_versioning.db_synthesis.baseline.SummaryChangeCounting.{subDomainInfo, subdomain}
+import de.hpi.dataset_versioning.data.metadata.custom.DatasetInfo
 import de.hpi.dataset_versioning.db_synthesis.baseline.decomposition.DecomposedTemporalTableCreationMain.args
 import de.hpi.dataset_versioning.io.IOService
 
@@ -8,6 +8,7 @@ object DecompositionCompletionMain extends App {
 
   IOService.socrataDir = args(0)
   val subdomain = args(1)
+  val subDomainInfo = DatasetInfo.readDatasetInfoBySubDomain
   val ids = subDomainInfo(subdomain)
     .map(_.id)
     .toIndexedSeq

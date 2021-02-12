5dd69d5346d292393024084cca82ea53e34316d6
leonbornemann <leonbornemann@gmx.de>
2021-02-12 21:50:06 +0100

    renamed class

--- src/main/scala/de/hpi/dataset_versioning/db_synthesis/preparation/AssociationMergeabilityGraphCreationMain.scala
+++ src/main/scala/de/hpi/dataset_versioning/db_synthesis/preparation/AssociationMergeabilityGraphCreationMain.scala
@@ -2,7 +2,7 @@ package de.hpi.dataset_versioning.db_synthesis.preparation
 
 import de.hpi.dataset_versioning.io.{DBSynthesis_IOService, IOService}
 
-object TupleMergeabilityGraphExploration extends App {
+object AssociationMergeabilityGraphCreationMain extends App {
 
   IOService.socrataDir = args(0)
   val subdomain = args(1)

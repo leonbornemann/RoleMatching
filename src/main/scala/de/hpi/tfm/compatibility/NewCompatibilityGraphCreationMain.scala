83d4389abbf1b8f541a92d2ccb601ac3971cc87a
leonbornemann <leonbornemann@gmx.de>
2021-03-26 17:47:04 +0100

    Introduced new indexing method that directly computes the edges while indexing

--- src/main/scala/de/hpi/tfm/compatibility/NewCompatibilityGraphCreationMain.scala
+++ src/main/scala/de/hpi/tfm/compatibility/NewCompatibilityGraphCreationMain.scala
@@ -1,9 +1,11 @@
 package de.hpi.tfm.compatibility
 
+import de.hpi.tfm.compatibility.AssociationGraphEdgeCandidateGenerationMain.subdomain
 import de.hpi.tfm.io.IOService
 
 object NewCompatibilityGraphCreationMain extends App {
   IOService.socrataDir = args(0)
   val subdomain = args(1)
-
+  new AssociationGraphEdgeCandidateGenerator(subdomain)
+    .serializeAllCandidatesNew()
 }

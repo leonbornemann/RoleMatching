2a83c7b04eb78fb61522f3b01d6d55a319f43e6b
leonbornemann <leonbornemann@gmx.de>
2021-03-30 11:07:54 +0200

    Further improved partitioing

--- src/main/scala/de/hpi/tfm/compatibility/graph/association/connected_component/AssociationEdgeCandidatePartitioner.scala
+++ src/main/scala/de/hpi/tfm/compatibility/graph/association/connected_component/AssociationEdgeCandidatePartitioner.scala
@@ -1,14 +1,15 @@
 package de.hpi.tfm.compatibility.graph.association.connected_component
 
+import com.typesafe.scalalogging.StrictLogging
 import de.hpi.tfm.compatibility.graph.association.AssociationGraphEdgeCandidate
 import de.hpi.tfm.io.{DBSynthesis_IOService, IOService}
 
 import java.io.PrintWriter
 
-object AssociationEdgeCandidatePartitioner extends App {
+object AssociationEdgeCandidatePartitioner extends App with StrictLogging{
   IOService.socrataDir = args(0)
   val subdomain = args(1)
-  val maxPartitionSize = 10
+  val maxPartitionSize = 20
   val candidateFile = DBSynthesis_IOService.getAssociationGraphEdgeCandidateFile(subdomain)
   val edges = AssociationGraphEdgeCandidate.fromJsonObjectPerLineFile(candidateFile.getAbsolutePath)
   assert(edges.toSet.size==edges.size)
@@ -24,30 +25,45 @@ object AssociationEdgeCandidatePartitioner extends App {
   val dir = DBSynthesis_IOService.getAssociationGraphEdgeCandidatePartitionDir(subdomain)
   var curPartitionNum = 0
   var totalSerializedEdges = 0
+  val curPartition = collection.mutable.ArrayBuffer[AssociationGraphEdgeCandidate]()
   adjacencyList.foreach{case (key,edges) => {
     val unseenEdges = edges.diff(representedEdges)
       .toIndexedSeq
     assert(unseenEdges.forall(e => !representedEdges.contains(e)))
-    val partitions = (0 until (unseenEdges.size / maxPartitionSize.toDouble).ceil.toInt)
-      .map(i => unseenEdges.slice(i*maxPartitionSize,(i+1)*maxPartitionSize))
-    partitions.foreach(p => {
-      val file = DBSynthesis_IOService.getAssociationGraphEdgeCandidatePartitionFile(subdomain,curPartitionNum)
-      val pr = new PrintWriter(file)
-      p.foreach(e => {
-        totalSerializedEdges +=1
-        e.appendToWriter(pr,false,true)
-        representedEdges += e
-      })
-      pr.close()
-      curPartitionNum +=1
+    unseenEdges.foreach(e => {
+      curPartition += e
+      if(curPartition.size>=maxPartitionSize){
+        clearAndSerializePartition
+      }
     })
   }}
+  if(curPartition.size>1)
+    clearAndSerializePartition
   if(totalSerializedEdges != edges.size){
     println(totalSerializedEdges)
     println(edges.size)
     assert(false)
   }
+  if(representedEdges.toSet == edges.toSet){
+    println("oh oh")
+    println(representedEdges.toSet.size)
+    println(edges.toSet.size)
+  }
+  assert(representedEdges.toSet == edges.toSet)
 
 
+  private def clearAndSerializePartition = {
+    val file = DBSynthesis_IOService.getAssociationGraphEdgeCandidatePartitionFile(subdomain, curPartitionNum)
+    val pr = new PrintWriter(file)
+    curPartition.foreach(e => {
+      totalSerializedEdges += 1
+      e.appendToWriter(pr, false, true)
+      representedEdges += e
+    })
+    pr.close()
+    logger.debug(s"Finished partition $curPartitionNum")
+    curPartitionNum += 1
+    curPartition.clear()
+  }
 
 }

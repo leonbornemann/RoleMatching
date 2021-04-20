e3991b850f11d4895a70fe67eb3de4847337c2ea
leonbornemann <leonbornemann@gmx.de>
2021-04-20 23:49:18 +0200

    Extended query to conjunctive query

--- src/main/scala/de/hpi/tfm/data/wikipedia/infobox/WikipediaCompatibilityForQueryMain.scala
+++ src/main/scala/de/hpi/tfm/data/wikipedia/infobox/WikipediaCompatibilityForQueryMain.scala
@@ -7,10 +7,11 @@ import de.hpi.tfm.data.tfmp_input.association.AssociationIdentifier
 import de.hpi.tfm.data.tfmp_input.table.nonSketch.FactLineage
 
 import java.io.{File, PrintWriter}
+import java.util.regex.Pattern
 
 object WikipediaCompatibilityForQueryMain extends App with StrictLogging {
 
-  val query = args(0)
+  val query = args(0).split(Pattern.quote("&")).toIndexedSeq
   val infoboxHistoryDir = new File(args(1))
   val queryResultDir = new File(args(2))
   val files = infoboxHistoryDir.listFiles()
@@ -19,14 +20,14 @@ object WikipediaCompatibilityForQueryMain extends App with StrictLogging {
   val fulfillsFilter = files.toIndexedSeq.flatMap(f => {
     val res = PaddedInfoboxHistory.fromJsonObjectPerLineFile(f.getAbsolutePath)
       .flatMap(ph => ph.lineages.map{case (p,h) => WikipediaInfoboxValueHistory(ph.pageID,ph.key,p,h)})
-      .filter(_.lineage.lineage.values.exists(_.toString.contains(query)))
+      .filter(wiwh => query.forall(s => wiwh.lineage.lineage.values.exists(_.toString.contains(s)))) //all query strings need to be matched in at least one value
     processed+=1
     if(processed%100==0)
       logger.debug(s"finished $processed")
     res
   })
-  logger.debug(s"Found ${fulfillsFilter.size} lineages containing $query")
-  val id = new AssociationIdentifier("wikipedia",s"contains $query",0,Some(0))
+  logger.debug(s"Found ${fulfillsFilter.size} lineages containing all terms in $query")
+  val id = new AssociationIdentifier("wikipedia",s"contains all in $query",0,Some(0))
   val attrID = 0
   val table = WikipediaInfoboxValueHistory.toAssociationTable(fulfillsFilter,id,attrID)
   val graphConfig = GraphConfig(0,InfoboxRevisionHistory.EARLIEST_HISTORY_TIMESTAMP,InfoboxRevisionHistory.LATEST_HISTORY_TIMESTAMP)
@@ -36,7 +37,7 @@ object WikipediaCompatibilityForQueryMain extends App with StrictLogging {
     .edges
     .map(e => WikipediaInfoboxValueHistoryMatch(fulfillsFilter(e.tupleReferenceA.rowIndex),fulfillsFilter(e.tupleReferenceB.rowIndex)))
   logger.debug("Finished compatibility graph creation")
-  val writer = new PrintWriter(queryResultDir.getAbsolutePath + s"/$query.json")
+  val writer = new PrintWriter(queryResultDir.getAbsolutePath + s"/${query.mkString("_AND_")}.json")
   edges.foreach(m => m.appendToWriter(writer,false,true))
   writer.close()
 

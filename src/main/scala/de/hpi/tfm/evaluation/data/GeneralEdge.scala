7d985487baaabba70c08da96fb98e9fc546fde04
leonbornemann <leonbornemann@gmx.de>
2021-05-10 09:23:27 +0200

    Introduced DirectEdgeFileEvaluation

--- src/main/scala/de/hpi/tfm/evaluation/data/GeneralEdge.scala
+++ src/main/scala/de/hpi/tfm/evaluation/data/GeneralEdge.scala
@@ -6,8 +6,8 @@ import de.hpi.tfm.evaluation.wikipediaStyle.GeneralEdgeStatRow
 
 case class GeneralEdge(v1:IdentifiedFactLineage, v2:IdentifiedFactLineage) extends JsonWritable[GeneralEdge] {
 
-  def toGeneralEdgeStatRow(i: Int, trainGraphConfig: GraphConfig) = {
-    GeneralEdgeStatRow(1,trainGraphConfig,v1.id,v2.id,v1.factLineage.toFactLineage,v2.factLineage.toFactLineage)
+  def toGeneralEdgeStatRow(granularityInDays: Int, trainGraphConfig: GraphConfig) = {
+    GeneralEdgeStatRow(granularityInDays,trainGraphConfig,v1.id,v2.id,v1.factLineage.toFactLineage,v2.factLineage.toFactLineage)
   }
 
 }
---
+++ src/main/scala/de/hpi/tfm/evaluation/wikipediaStyle/DirectEdgeFileEvaluation.scala
@@ -0,0 +1,28 @@
+package de.hpi.tfm.evaluation.wikipediaStyle
+
+import de.hpi.tfm.compatibility.GraphConfig
+import de.hpi.tfm.evaluation.data.{GeneralEdge, IdentifiedFactLineage}
+import de.hpi.tfm.evaluation.wikipediaStyle.EdgeBasedEvaluationWikipediaStyleMain.{args, minEvidence}
+
+import java.io.PrintWriter
+import java.time.LocalDate
+
+object DirectEdgeFileEvaluation extends App {
+  val edgeFile = args(0)
+  val resultFile = args(1)
+  val timeRangeStart = LocalDate.parse(args(2))
+  val timeRangeEnd = LocalDate.parse(args(3))
+  val trainGraphConfig = GraphConfig(minEvidence,timeRangeStart,timeRangeEnd)
+  val edges = GeneralEdge.fromJsonObjectPerLineFile(edgeFile)
+  val resultPR = new PrintWriter(resultFile)
+  edges.zipWithIndex.foreach{case (e,i) => {
+    val statRow = e.toGeneralEdgeStatRow(1,trainGraphConfig)
+    if(i==0)
+      resultPR.println(statRow.getSchema.mkString(","))
+    resultPR.println(statRow.toCSVLine)
+  }}
+  resultPR.close()
+  //timeRangeStartTrain="2019-11-01"
+  //timeRangeEndTrain="2020-04-30"
+
+}

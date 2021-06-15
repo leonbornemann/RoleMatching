3d7b516280231ada68d825bca7ef4074388e7342
leonbornemann <leonbornemann@gmx.de>
2021-06-15 16:55:52 +0200

    fixed stupid json bug by avoid negative infiniti (fixed)

--- src/main/scala/de/hpi/tfm/fact_merging/optimization/GraphOptimizationMain.scala
+++ src/main/scala/de/hpi/tfm/fact_merging/optimization/GraphOptimizationMain.scala
@@ -11,26 +11,27 @@ import java.io.File
 import java.time.LocalDate
 
 object GraphOptimizationMain extends App with StrictLogging{
-  val edgeFile = args(0)
-  val TIMESTAMP_RESOLUTION_IN_DAYS = args(1).toInt
-  val timeStart = LocalDate.parse(args(2))
-  val trainTimeEnd = LocalDate.parse(args(3))
-  val timeEnd = LocalDate.parse(args(4))
-  val metricThreshold = args(5).toDouble
-  val slimGraphFile = new File(args(6))
-  val resultFile = new File(args(7)) // get real path somewhere!
-  assert(resultFile.getParentFile.exists())
-  IOService.STANDARD_TIME_FRAME_START=timeStart
-  IOService.STANDARD_TIME_FRAME_END=timeEnd
-  val distanceMetric:EdgeScore[Any] = ???//new MultipleEventWeightScore(TIMESTAMP_RESOLUTION_IN_DAYS,trainTimeEnd)
-  logger.debug("Loading edges")
-  var edges = GeneralEdge.fromJsonObjectPerLineFile(args(0)).toIndexedSeq
-  logger.debug("Finished loading edges")
-  //val histogramMode: TransitionHistogramMode = TransitionHistogramMode.COUNT_NON_CHANGE_ONLY_ONCE //get these as params!
-  val slimGraph = SlimGraphOld.fromIdentifiedEdges(edges,distanceMetric) //or load this directly
-  slimGraph.toJsonFile(slimGraphFile)
-  edges=null
-  logger.debug("Finished transformation to slim graph")
-  val optimizer = new GreedyEdgeBasedOptimizer(slimGraph.transformToOptimizationGraph, resultFile, metricThreshold) //TODO: get threshold somewere!
-  optimizer.runComponentWiseOptimization()
+  //TODO: this needs to be changed!
+  //  val edgeFile = args(0)
+//  val TIMESTAMP_RESOLUTION_IN_DAYS = args(1).toInt
+//  val timeStart = LocalDate.parse(args(2))
+//  val trainTimeEnd = LocalDate.parse(args(3))
+//  val timeEnd = LocalDate.parse(args(4))
+//  val metricThreshold = args(5).toDouble
+//  val slimGraphFile = new File(args(6))
+//  val resultFile = new File(args(7)) // get real path somewhere!
+//  assert(resultFile.getParentFile.exists())
+//  IOService.STANDARD_TIME_FRAME_START=timeStart
+//  IOService.STANDARD_TIME_FRAME_END=timeEnd
+//  val distanceMetric:EdgeScore[Any] = ???//new MultipleEventWeightScore(TIMESTAMP_RESOLUTION_IN_DAYS,trainTimeEnd)
+//  logger.debug("Loading edges")
+//  var edges = GeneralEdge.fromJsonObjectPerLineFile(args(0)).toIndexedSeq
+//  logger.debug("Finished loading edges")
+//  //val histogramMode: TransitionHistogramMode = TransitionHistogramMode.COUNT_NON_CHANGE_ONLY_ONCE //get these as params!
+//  val slimGraph = SlimGraphOld.fromIdentifiedEdges(edges,distanceMetric) //or load this directly
+//  slimGraph.toJsonFile(slimGraphFile)
+//  edges=null
+//  logger.debug("Finished transformation to slim graph")
+//  val optimizer = new GreedyEdgeBasedOptimizer(slimGraph.transformToOptimizationGraph, resultFile) //TODO: get threshold somewere!
+//  optimizer.runComponentWiseOptimization()
 }

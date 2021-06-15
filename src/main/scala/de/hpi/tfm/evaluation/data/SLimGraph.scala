fa4315c86140a45bc78d19276e777f87d9a857cc
leonbornemann <leonbornemann@gmx.de>
2021-06-15 15:21:00 +0200

    Simplified SLimGraph creation to work iteratively (changed to have floats)

--- src/main/scala/de/hpi/tfm/evaluation/data/SLimGraph.scala
+++ src/main/scala/de/hpi/tfm/evaluation/data/SLimGraph.scala
@@ -13,7 +13,28 @@ import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
 //adjacency list only contains every edge once (from the lower index to the higher index)! First element of adjacency list is the node index, second is the score
-case class SLimGraph(verticesOrdered: IndexedSeq[String], adjacencyList: collection.Map[Int, collection.Map[Int, Int]]) extends JsonWritable[SLimGraph] with StrictLogging{
+case class SLimGraph(verticesOrdered: IndexedSeq[String], adjacencyList: collection.Map[Int, collection.Map[Int, Float]]) extends JsonWritable[SLimGraph] with StrictLogging{
+
+  val scoreRangeIntMin = Integer.MIN_VALUE / 1000.0
+  val scoreRangeIntMax = Integer.MAX_VALUE / 1000.0
+  val scoreRangeDoubleMin = -1.0.toFloat
+  val scoreRangeDoubleMax = 1.0.toFloat
+
+  //Tranfer x from scale [a,b] to y in scale [c,d]
+  // (x-a) / (b-a) = (y-c) / (d-c)
+  //
+  //y = (d-c)*(x-a) / (b-a) +c
+  def scaleInterpolation(x: Double, a: Double, b: Double, c: Double, d: Double) = {
+    val y = (d-c)*(x-a) / (b-a) +c
+    assert(y >=c && y <= d)
+    y
+  }
+
+  def getScoreAsInt(weight:Float):Int = {
+    assert( weight.isNegInfinity || weight >= scoreRangeDoubleMin && weight <= scoreRangeDoubleMax)
+    val scoreAsInt = if(weight.isNegInfinity) Integer.MIN_VALUE else scaleInterpolation(weight,scoreRangeDoubleMin,scoreRangeDoubleMax,scoreRangeIntMin,scoreRangeIntMax).round.toInt
+    scoreAsInt
+  }
 
   //serializes this as an adjacencyMatrix file
   def serializeToMDMCPInputFile(f:File) = {
@@ -22,10 +43,10 @@ case class SLimGraph(verticesOrdered: IndexedSeq[String], adjacencyList: collect
     verticesOrdered
       .zipWithIndex
       .foreach{case (_,i) => {
-        val neighbors = adjacencyList.getOrElse(i,Map[Int,Int]())
-        val weights = (i until verticesOrdered.size).map{ j =>
-          val weight = neighbors.getOrElse(j,Integer.MIN_VALUE)
-          weight
+        val neighbors = adjacencyList.getOrElse(i,Map[Int,Float]())
+        val weights = Seq(0) ++ ((i+1) until verticesOrdered.size).map{ j =>
+          val weight:Float = neighbors.getOrElse(j,Float.NegativeInfinity)
+          getScoreAsInt(weight)
         }
         pr.println(weights.mkString("  "))
         if(i%1000==0){
@@ -38,42 +59,21 @@ case class SLimGraph(verticesOrdered: IndexedSeq[String], adjacencyList: collect
 }
 object SLimGraph extends JsonReadable[SLimGraph] with StrictLogging {
 
-  val scoreRangeIntMin = Integer.MIN_VALUE / 1000.0
-  val scoreRangeIntMax = Integer.MAX_VALUE / 1000.0
-  val scoreRangeDoubleMin = -1.0
-  val scoreRangeDoubleMax = 1.0
-
-  //Tranfer x from scale [a,b] to y in scale [c,d]
-  // (x-a) / (b-a) = (y-c) / (d-c)
-  //
-  //y = (d-c)*(x-a) / (b-a) +c
-  def scaleInterpolation(x: Double, a: Double, b: Double, c: Double, d: Double) = {
-    val y = (d-c)*(x-a) / (b-a) +c
-    assert(y >=c && y <= d)
-    y
-  }
-
-  def getScoreAsInt(e: GeneralEdge, scoringFunction: MultipleEventWeightScore[Any], scoringFunctionThreshold: Double):Int = {
-    val score = scoringFunction.compute(e.v1.factLineage.toFactLineage,e.v2.factLineage.toFactLineage)
-    val doubleWeightCorrected = score-scoringFunctionThreshold
-    assert( doubleWeightCorrected.isNegInfinity || doubleWeightCorrected >= scoreRangeDoubleMin && doubleWeightCorrected <= scoreRangeDoubleMax)
-    val scoreAsInt = if(doubleWeightCorrected.isNegInfinity) Integer.MIN_VALUE else scaleInterpolation(doubleWeightCorrected,scoreRangeDoubleMin,scoreRangeDoubleMax,scoreRangeIntMin,scoreRangeIntMax).round.toInt
-    scoreAsInt
-  }
-
   def fromGeneralEdgeIterator(edges: hpi.tfm.evaluation.data.GeneralEdge.JsonObjectPerLineFileIterator, scoringFunction: MultipleEventWeightScore[Any], scoringFunctionThreshold: Double) = {
     val vertices = collection.mutable.HashSet[String]()
-    val adjacencyList = collection.mutable.HashMap[String, mutable.HashMap[String, Int]]()
+    val adjacencyList = collection.mutable.HashMap[String, mutable.HashMap[String, Float]]()
     var count = 0
     edges.foreach(e => {
       vertices.add(e.v1.id)
       vertices.add(e.v2.id)
-      val scoreAsInt = getScoreAsInt(e,scoringFunction,scoringFunctionThreshold)
+      val score = scoringFunction.compute(e.v1.factLineage.toFactLineage,e.v2.factLineage.toFactLineage)
+      val weight = (score-scoringFunctionThreshold).toFloat
+      //val scoreAsInt = getScoreAsInt(e,scoringFunction,scoringFunctionThreshold)
       assert(e.v1.id!=e.v2.id)
       if(e.v1.id<e.v2.id)
-        adjacencyList.getOrElseUpdate(e.v1.id,mutable.HashMap[String,Int]()).put(e.v2.id,scoreAsInt)
+        adjacencyList.getOrElseUpdate(e.v1.id,mutable.HashMap[String,Float]()).put(e.v2.id,weight)
       else {
-        adjacencyList.getOrElseUpdate(e.v2.id,mutable.HashMap[String,Int]()).put(e.v1.id,scoreAsInt)
+        adjacencyList.getOrElseUpdate(e.v2.id,mutable.HashMap[String,Float]()).put(e.v1.id,weight)
       }
       count+=1
       if(count%100000==0)

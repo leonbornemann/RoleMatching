6da8ffa0c1f5e1594abc1b4e24cc3cf2b0e2b270
leonbornemann <leonbornemann@gmx.de>
2021-06-23 10:07:20 +0200

    More debugging! (fixed)

--- src/main/scala/de/hpi/tfm/evaluation/data/ConnectedComponentBasedOptimizationMain.scala
+++ src/main/scala/de/hpi/tfm/evaluation/data/ConnectedComponentBasedOptimizationMain.scala
@@ -8,30 +8,6 @@ import scalax.collection.edge.WUnDiEdge
 import java.io.File
 
 object ConnectedComponentBasedOptimizationMain extends App {
-//  new BruteForceComponentOptimizer(null,IndexedSeq(0)).optimize()
-//  println("-----------------------------------------------------------------------------------")
-//  new BruteForceComponentOptimizer(null,IndexedSeq(0,1)).optimize()
-//  println("-----------------------------------------------------------------------------------")
-//  new BruteForceComponentOptimizer(null,IndexedSeq(0,1,2)).optimize()
-//  println("-----------------------------------------------------------------------------------")
-  new BruteForceComponentOptimizer(null,IndexedSeq(1,2,3,4,5,6,7,8)).optimize()
-  println("-----------------------------------------------------------------------------------")
-//  case class lol(a:Double) extends JsonWritable[lol]
-//  object lol extends JsonReadable[lol]
-//
-//  val a = lol(Double.NegativeInfinity)
-//  private val jsonString: String = a.toJson()
-//  println(jsonString)
-//  val b = lol.fromJsonString(jsonString)
-//  println(b)
-
-  //test this a bit
-//  val vertices = Seq(4,7,2,3,100)
-//  val edges = Set(WUnDiEdge(2,3)(-0.7),WUnDiEdge(3,4)(0.5),WUnDiEdge(4,7)(-0.5),WUnDiEdge(7,100)(0.2))
-//  val graph1 = Graph.from(vertices,edges)
-//  val subGraph = new SubGraph(graph1)
-//  subGraph.toMDMCPInputFile(new File("testNew.txt"))
-//  assert(false)
   val slimGraphFile = args(0)
   val resultFile = new File(args(1))
   val mdmcpExportDir = new File(args(2))

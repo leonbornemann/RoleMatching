44c53ba443682f98728097c326b49fda27dff7eb
leonbornemann <leonbornemann@gmx.de>
2021-06-28 15:50:57 +0200

    Fixed bug in tuning code

--- src/main/scala/de/hpi/tfm/fact_merging/optimization/tuning/StatisticBasedWeightTuningMain.scala
+++ src/main/scala/de/hpi/tfm/fact_merging/optimization/tuning/StatisticBasedWeightTuningMain.scala
@@ -2,6 +2,8 @@ package de.hpi.tfm.fact_merging.optimization.tuning
 
 import de.hpi.tfm.evaluation.data.{GeneralEdge, IdentifiedFactLineage, SlimGraphWithoutWeight}
 import de.hpi.tfm.fact_merging.metrics.{MultipleEvenWeightStatCounter, MultipleEventWeightScore, MultipleEventWeightScoreComputer}
+import de.hpi.tfm.fact_merging.optimization.GeneralEdgeGraphToSlimGraph.{timeEnd, timeStart}
+import de.hpi.tfm.io.IOService
 
 import java.io.File
 import java.time.LocalDate
@@ -12,6 +14,10 @@ object StatisticBasedWeightTuningMain extends App {
   val resultDir = new File(args(2))
   resultDir.mkdir()
   val TIMESTAMP_GRANULARITY_IN_DAYS = args(3).toInt
+  val timeStart = LocalDate.parse(args(4))
+  val timeEnd = LocalDate.parse(args(5))
+  IOService.STANDARD_TIME_FRAME_START=timeStart
+  IOService.STANDARD_TIME_FRAME_END=timeEnd
   val graph = SlimGraphWithoutWeight.fromJsonFile(inputFile.getAbsolutePath)
   val aggregator = new MultipleEvenWeightStatCounter(dsName,graph,TIMESTAMP_GRANULARITY_IN_DAYS)
   val counts = aggregator.aggregateEventCounts()

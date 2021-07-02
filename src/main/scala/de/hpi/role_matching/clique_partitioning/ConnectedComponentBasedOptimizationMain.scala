d9739961fd47b66c94ee7b919a18da8e412848e5
leonbornemann <leonbornemann@gmx.de>
2021-07-02 10:58:13 +0200

    added CLIArgs print

--- src/main/scala/de/hpi/role_matching/clique_partitioning/ConnectedComponentBasedOptimizationMain.scala
+++ src/main/scala/de/hpi/role_matching/clique_partitioning/ConnectedComponentBasedOptimizationMain.scala
@@ -1,12 +1,14 @@
 package de.hpi.role_matching.clique_partitioning
 
 
+import com.typesafe.scalalogging.StrictLogging
 import de.hpi.role_matching.compatibility.graph.representation.slim.{SLimGraph, SlimGraphSet, SlimGraphWithoutWeight}
 
 import java.io.File
 import java.time.LocalDate
 
-object ConnectedComponentBasedOptimizationMain extends App {
+object ConnectedComponentBasedOptimizationMain extends App with StrictLogging{
+  logger.debug(s"Called with ${args.toIndexedSeq}")
   val inputGraphFile = args(0)
   val trainTimeEnd = LocalDate.parse(args(1))
   val weightConfig = ScoreConfig.fromCLIArguments(args(2),args(3))
@@ -14,10 +16,12 @@ object ConnectedComponentBasedOptimizationMain extends App {
   var graph = SlimGraphSet.fromJsonFile(inputGraphFile)
   val optimizationGraph = graph.transformToOptimizationGraph(trainTimeEnd,weightConfig)
   graph = null //might help out the garbage collector
-  val resultDir = new File(args(5))
-  val mdmcpExportDir = new File(args(6))
-  val vertexLookupDirForPartitions = new File(args(7))
-  val greedyMergeDir = new File(args(8))
+  private val alphaDirName = s"/alpha_${args(3)}/"
+  val resultDir = new File(args(5) + alphaDirName)
+  val mdmcpExportDir = new File(args(6) + alphaDirName)
+  val vertexLookupDirForPartitions = new File(args(7) + alphaDirName)
+  val greedyMergeDir = new File(args(8) + alphaDirName)
+  Seq(resultDir,mdmcpExportDir,vertexLookupDirForPartitions,greedyMergeDir).foreach(_.mkdirs())
   val optimizer = new HybridOptimizer(optimizationGraph, resultDir, mdmcpExportDir, vertexLookupDirForPartitions, greedyMergeDir,runGreedyOnly)
   optimizer.runComponentWiseOptimization()
   //optimizer.printComponentSizeHistogram()

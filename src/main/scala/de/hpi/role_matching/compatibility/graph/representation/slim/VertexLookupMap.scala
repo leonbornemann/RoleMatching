a3e1c026823b295ea71f6d7713722155e90cef59
leonbornemann <leonbornemann@gmx.de>
2021-07-20 16:18:12 +0200

    Introduced VertexLookupMap and changed evaluations and optiimization to work with SlimGraphSet (fixed)

--- src/main/scala/de/hpi/role_matching/compatibility/graph/representation/slim/VertexLookupMap.scala
+++ src/main/scala/de/hpi/role_matching/compatibility/graph/representation/slim/VertexLookupMap.scala
@@ -3,7 +3,7 @@ package de.hpi.role_matching.compatibility.graph.representation.slim
 import de.hpi.role_matching.compatibility.graph.representation.vertex.IdentifiedFactLineage
 import de.hpi.socrata.{JsonReadable, JsonWritable}
 
-case class VertexLookupMap(vertexNamesOrdered: IndexedSeq[String], private val posToLineage:Map[Int,IdentifiedFactLineage]) extends JsonWritable[VertexLookupMap]{
+case class VertexLookupMap(vertexNamesOrdered: IndexedSeq[String], posToLineage:Map[Int,IdentifiedFactLineage]) extends JsonWritable[VertexLookupMap]{
   vertexNamesOrdered.zipWithIndex.foreach(t => assert(posToLineage(t._2).id==t._1))
 
   val posToFactLineage = posToLineage.map(t => (t._1,t._2.factLineage.toFactLineage))

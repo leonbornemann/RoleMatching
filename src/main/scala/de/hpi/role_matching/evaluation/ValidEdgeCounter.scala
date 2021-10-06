18e30c1caaf89e7cdaba056d4ebac1bd8719e8d2
leonbornemann <leonbornemann@gmx.de>
2021-10-06 22:31:56 +0200

    Added code for new recall counting

--- src/main/scala/de/hpi/role_matching/evaluation/ValidEdgeCounter.scala
+++ src/main/scala/de/hpi/role_matching/evaluation/ValidEdgeCounter.scala
@@ -1,16 +1,20 @@
 package de.hpi.role_matching.evaluation
 
+import com.typesafe.scalalogging.StrictLogging
 import de.hpi.role_matching.compatibility.graph.representation.slim.SlimGraphWithoutWeight
 
 import java.time.LocalDate
 
-case class ValidEdgeCounter(graph: SlimGraphWithoutWeight, trainTimeEnd: LocalDate) extends StatComputer {
+case class ValidEdgeCounter(graph: SlimGraphWithoutWeight, trainTimeEnd: LocalDate) extends StatComputer with StrictLogging{
 
   def printCount() = {
     val lineageMap = graph.verticesOrdered.map(_.factLineage.toFactLineage)
     var truePositiveCount = 0
     var trueButUninterestingCount = 0
     var invalidEdgeCount = 0
+    val allNodes = graph.adjacencyList.size
+    var processed = 0
+    val step = allNodes / 1000
     graph.adjacencyList.foreach{case (originID,list) => {
       list
         .withFilter(t => t._2.size == graph.trainTimeEnds.size)
@@ -30,6 +34,10 @@ case class ValidEdgeCounter(graph: SlimGraphWithoutWeight, trainTimeEnd: LocalDa
             throw new AssertionError() //should never end up here
           }
         }}
+      processed+=1
+      if(processed%step ==0){
+        logger.debug(s"Processed neighbours of $processed nodes (${100*processed/allNodes.toDouble}%)")
+      }
     }}
     println("truePositiveCount:"+truePositiveCount)
     println("trueButUninterestingCount:"+trueButUninterestingCount)

3652b295c06490c31d014f427877ec87334a06e2
leonbornemann <leonbornemann@gmx.de>
2021-11-11 15:13:29 +0100

    Finished new executable shell scripts to facilitate execution

--- src/main/resources/executables/roleMatchingEvaluation.sh
+++ src/main/resources/executables/roleMatchingEvaluation.sh
@@ -1,9 +1,12 @@
-#input directories
-graphDir="<substitute the path to a directory containing the graph set files (output of tuning Data Export)>"
+#input directories:
 rolesetDir="<substitute the path to a diretory contained the role sets json files here>"
+inputRoleMatchingDir="<substitute the path to a directory for the output here>"
+inputRoleMatchingFromMDMCPDir="<substitute the path to a directory for the output here>"
+vertexMappingDirMDMCP="<substitute the path to a directory for the output here>"
+graphDir="<substitute the path to a directory for the output here>"
 resultDir="<substitute the path to a directory for the output here>"
 jarFile="<substitute the path to the jar file obtained by sbt assembly here>"
-
+#further config
 weightSettingSocrata="../weightSettings/socrata.json"
 weightSettingWikipedia="../weightSettings/wikipedia.json"
 weightSettings=( "$weightSettingSocrata" "$weightSettingSocrata" "$weightSettingSocrata" "$weightSettingSocrata" "$weightSettingSocrata" "$weightSettingWikipedia" "$weightSettingWikipedia" "$weightSettingWikipedia" "$weightSettingWikipedia" "$weightSettingWikipedia")
@@ -18,16 +21,19 @@ for i in "${!datasetNames[@]}";
 do
         datasetName=${datasetNames[i]}
         #setting up result directories:
-        currentResultDir="$resultDir/$datasetName/"
+        dataSource=${dataSources[i]}
+        currentInputRoleMatchingDir="$inputRoleMatchingDir/$datasetName/"
+        currentInputRoleMatchingDirFromMDMCP="$inputRoleMatchingFromMDMCPDir/$datasetName/"
+        currentVertexMappingDir="$vertexMappingDirMDMCP/$datasetName/"
+        graphFile="$graphDir/$datasetName.json/"
         matchingEndTime=${matchingEndTimes[i]}
         weightConfig=${weightSettings[i]}
-        currentResultDir="$resultDir/$datasetName/sgcp/"
+        currentResultDir="$resultDir/$datasetName/"
         rolesetFile="$rolesetDir/$datasetName.json"
         mkdir currentResultDir
-        logFile="logs/${datasetName}_sgcp.log"
-        dataSource=${dataSources[i]}
+        logFile="logs/${datasetName}_evaluation.log"
         #starting the process
         echo "Running $datasetName with nThreads $nThreads"
-        java -ea -Xmx96g -cp $jarFile de.hpi.role_matching.cbrm.sgcp.SparseGraphCliquePartitioningMain $dataSource $graphsetFile $trainTimeEnd $weightConfig $resultDir $rolesetFile  > $logFile 2<&1
+        java -ea -Xmx96g -cp $jarFile de.hpi.role_matching.cbrm.compatibility_graph.role_tree.CompatibilityGraphCreationMain $dataSource $currentInputRoleMatchingDir $currentInputRoleMatchingDirFromMDMCP $currentVertexMappingDir $graphFile $matchingEndTime $weightConfig $currentResultDir $rolesetFile > $logFile 2<&1
         echo "Finished with exit code $?"
 done

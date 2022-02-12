08ff5fddea183dddd47d9298260f8b4d10f2c560
leonbornemann <leonbornemann@gmx.de>
2022-02-12 12:06:03 +0100

    Added Ditto blocking stage Added roleset extraction from wikipedia templates

--- src/main/scala/de/hpi/role_matching/ditto/DittoExporter.scala
+++ src/main/scala/de/hpi/role_matching/ditto/DittoExporter.scala
@@ -1,5 +1,6 @@
 package de.hpi.role_matching.ditto
 
+import com.typesafe.scalalogging.StrictLogging
 import de.hpi.role_matching.GLOBAL_CONFIG
 import de.hpi.role_matching.cbrm.data.{RoleLineageWithID, Roleset}
 import de.hpi.role_matching.evaluation.tuning.BasicStatRow
@@ -7,31 +8,40 @@ import de.hpi.role_matching.evaluation.tuning.BasicStatRow
 import java.io.{File, PrintWriter}
 import java.time.LocalDate
 
-class DittoExporter(vertices: Roleset, trainTimeEnd: LocalDate,resultFile:File) {
+class DittoExporter(vertices: Roleset, trainTimeEnd: LocalDate,resultFile:File) extends StrictLogging{
 
   val vertexMap = vertices.getStringToLineageMap.map{case (k,v) => (k,v.roleLineage.toRoleLineage)}
   val vertexMapOnlyTrain = vertexMap.map{case (k,v) => (k,v.projectToTimeRange(GLOBAL_CONFIG.STANDARD_TIME_FRAME_START,trainTimeEnd))}
+  val vertexMapOnlyTrainWithID = vertices.getStringToLineageMap.map{case (k,v) => (k,RoleLineageWithID(v.id,v.roleLineage.toRoleLineage.projectToTimeRange(GLOBAL_CONFIG.STANDARD_TIME_FRAME_START,trainTimeEnd).toSerializationHelper))}
+
+
   val resultPr = new PrintWriter(resultFile)
 
   def exportData() = {
     val blocks:IndexedSeq[IndexedSeq[String]] = blocking()
-    blocks.foreach{ block =>
-      for(i <- 0 until block.size){
-        for(j <- i until block.size){
-          val v1 = block(i)
-          val v2 = block(j)
-          val label:Option[Boolean] = getClassLabel(v1,v2)
-          if(label.isDefined){
-            outputRecord(v1,v2,label.get)
-          }
-        }
-      }
-    }
-    resultPr.close()
+//    blocks.foreach{ block =>
+//      for(i <- 0 until block.size){
+//        for(j <- i until block.size){
+//          val v1 = block(i)
+//          val v2 = block(j)
+//          val label:Option[Boolean] = getClassLabel(v1,v2)
+//          if(label.isDefined){
+//            outputRecord(v1,v2,label.get)
+//          }
+//        }
+//      }
+//    }
+//    resultPr.close()
   }
 
   def blocking(): IndexedSeq[IndexedSeq[String]] = {
-    ???
+    val blocks = vertexMapOnlyTrainWithID
+      .groupBy(t => vertexMapOnlyTrain(t._1).nonWildCardValues.toSet)
+      .map{case (k,v) => v.values.map(_.id).toIndexedSeq}
+      .toIndexedSeq
+    val totalNumberOfEdges = blocks.map(b => (b.size*(b.size-1))/2).sum
+    logger.debug(s"Will create $totalNumberOfEdges")
+    blocks
   }
 
   def outputRecord(id1:String, id2:String, label: Boolean) = {
---
+++ src/main/scala/de/hpi/wikipedia_data_preparation/TemplateDirToRolesetMain.scala
@@ -0,0 +1,39 @@
+package de.hpi.wikipedia_data_preparation
+
+import de.hpi.role_matching.cbrm.data.{RoleLineage, RoleLineageWithID, Roleset}
+
+import java.io.File
+import scala.io.Source
+
+object TemplateDirToRolesetMain extends App {
+  val templateRootDir = new File(args(0))
+  val rolesetRootDir = new File(args(1))
+  val datasetList = Source.fromFile(args(2))
+    .getLines()
+    .toIndexedSeq
+    .map(s => {
+      val tokens = s.split(":")
+      val name = tokens(0)
+      val templates = tokens(1).split(",")
+      (name,templates)
+    })
+    .toMap
+  val configDirs = templateRootDir.listFiles()
+  configDirs.foreach(dir => {
+    val configName = dir.getName
+    val resultDir = new File(rolesetRootDir.getAbsolutePath + s"/$configName/")
+    resultDir.mkdirs()
+    datasetList.foreach{case (dsName,templates) => {
+      val resultFile = new File(resultDir.getAbsolutePath + s"/$dsName.json")
+      val roles = templates.flatMap(template => RoleLineageWithID.fromJsonObjectPerLineFile(dir.getAbsolutePath + s"/$template.json"))
+        .map(rl => (rl.id,rl))
+        .toIndexedSeq
+        .sortBy(_._1)
+      val roleIDs = roles.map(_._1)
+      val posToRLMap = roles.zipWithIndex.map{case ((_,rl),i) => (i,rl)}.toMap
+      val roleset = Roleset(roleIDs,posToRLMap)
+      roleset.toJsonFile(resultFile)
+    }}
+  })
+  val resultFile = args(1)
+}

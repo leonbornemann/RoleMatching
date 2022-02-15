a6b78709c9a6664669a7e70747647243be491a8e
leonbornemann <leonbornemann@gmx.de>
2022-02-15 13:06:36 +0100

    Added super strict label for evaluation

--- src/main/scala/de/hpi/role_matching/evaluation/tuning/BasicStatRow.scala
+++ src/main/scala/de/hpi/role_matching/evaluation/tuning/BasicStatRow.scala
@@ -1,5 +1,6 @@
 package de.hpi.role_matching.evaluation.tuning
 
+import de.hpi.role_matching.GLOBAL_CONFIG
 import de.hpi.role_matching.cbrm.compatibility_graph.representation.simple.SimpleCompatbilityGraphEdge
 import de.hpi.role_matching.cbrm.data.RoleLineage
 import de.hpi.role_matching.evaluation.matching.StatComputer
@@ -13,4 +14,10 @@ class BasicStatRow(val fl1:RoleLineage,val fl2:RoleLineage,val trainTimeEnd:Loca
   def remainsValidFullTimeSpan = fl1.tryMergeWithConsistent(fl2, RemainsValidVariant.STRICT).isDefined
 
   def interestingnessEvidence = getEvidenceInTestPhase(fl1, fl2, trainTimeEnd)
+
+  def isValidSuperStrict = {
+    val fl1Projected = fl1.projectToTimeRange(trainTimeEnd,GLOBAL_CONFIG.STANDARD_TIME_FRAME_END).lineage.filter(!RoleLineage.isWildcard(_))
+    val fl2Projected = fl2.projectToTimeRange(trainTimeEnd,GLOBAL_CONFIG.STANDARD_TIME_FRAME_END).lineage.filter(!RoleLineage.isWildcard(_))
+    fl1Projected == fl2Projected
+  }
 }

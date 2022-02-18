5f4d52f14ada46671c5010a4aa82b36c005e14bc
leonbornemann <leonbornemann@gmx.de>
2022-02-18 23:28:27 +0100

    Added new filtering methods Introduced template stat count

--- src/main/scala/de/hpi/wikipedia_data_preparation/transformed/WikipediaRoleLineageExtractionMain.scala
+++ src/main/scala/de/hpi/wikipedia_data_preparation/transformed/WikipediaRoleLineageExtractionMain.scala
@@ -31,6 +31,7 @@ object WikipediaRoleLineageExtractionMain extends App with StrictLogging {
     revisionHistories
       .foreach(r => {
         val res = r.toWikipediaInfoboxValueHistories.toIndexedSeq
+        println(s"Found ${r.revisions.flatMap(_.template).size} templates")
         val weird = res.filter(_.lineage.lineage.keySet.exists(_.isAfter(InfoboxRevisionHistory.LATEST_HISTORY_TIMESTAMP)))
         assert(weird.size==0)
         val retained = res.filter(vh => {

9e4734b59e06d006e4929f392dfb3959cc25b8b0
leonbornemann <leonbornemann@gmx.de>
2022-09-26 23:48:21 +0200

    Fixed MissingDataRolesetGenerator

--- src/main/scala/de/hpi/role_matching/evaluation/blocking/ground_truth/LabelledRoleMatchingMissingValueEvaluation.scala
+++ src/main/scala/de/hpi/role_matching/evaluation/blocking/ground_truth/LabelledRoleMatchingMissingValueEvaluation.scala
@@ -4,7 +4,6 @@ import de.hpi.util.GLOBAL_CONFIG
 
 import java.io.{File, PrintWriter}
 
-//java -ea -Xmx100g -cp TODO de.hpi.role_matching.evaluation.blocking.ground_truth.LabelledRoleMatchingMissingValueEvaluation /san2/data/change-exploration/roleMerging/finalExperiments/ground_truth/dgs_cleaned/
 object LabelledRoleMatchingMissingValueEvaluation extends App {
   GLOBAL_CONFIG.setSettingsForDataSource("wikipedia")
   val inputLabelDirs = new File(args(0)).listFiles()
@@ -14,7 +13,7 @@ object LabelledRoleMatchingMissingValueEvaluation extends App {
   val targetBasename = new File(args(0)).getName
   rolesetRootDir.listFiles.foreach(d => {
     println(d)
-    val pr = new PrintWriter(resultDir + s"/${targetBasename}_$d")
+    val pr = new PrintWriter(resultDir + s"/${targetBasename}_$d.csv")
     val rolesetFiles = d.listFiles()
     val RoleMatchEvaluator = new RoleMatchEvaluator(rolesetFiles)
     RoleMatchEvaluator.executeEvaluation(inputLabelDirs,pr)
